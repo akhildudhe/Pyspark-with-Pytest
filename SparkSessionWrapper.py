ca8591a286c18e00f096f0912cc13e7f88e2ddd2
akhildudhe <akhil.s.dudhe@gmail.com>
2020-05-30 20:48:28 +0530

    initial_project_upload

---
+++ SparkSessionWrapper.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+import configparser
+import sys
+
+def SparkSession_initialize(environment):
+
+	config = configparser.ConfigParser()
+	config.read('application.properties')
+
+	execution_mode = config.get(environment,'exection.mode')
+	appname        = config.get(environment,'appname')
+	executormemory = config.get(environment,'executormemory')
+	executorcores  = config.get(environment,'executorcores')
+	deploymode     = config.get(environment,'deploymode')
+	hivemetastore  = config.get(environment,'hivemetastore')
+	warehousedir   = config.get(environment,'warehousedir')
+
+
+	inputdir       = config.get(environment,'input.base.dir')
+	outputdir      = config.get(environment,'output.base.dir')
+ 
+	spark = SparkSession.builder.master(execution_mode).appName(appname).getOrCreate()
+
+	return [spark,inputdir,output]
